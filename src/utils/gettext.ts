99c6f871906dc063f55673aa8192eb682dfc2ca0
Fedor Burhonov <fedor.burgonov@mdfin.com>
2020-07-13 15:38:15 +0300

    Added fonts

--- src/utils/gettext.ts
+++ src/utils/gettext.ts
@@ -3,18 +3,20 @@ import GetText from 'node-gettext';
 import { defaultLocale, locales, defaultLocaleDomain } from './../config.json';
 import { TJSON } from '../interfaces';
 
-import uaLocale from './../locales/uk-UK/common.po';
-import ruLocale from './../locales/ru-RU/common.po';
+import viVN from './../locales/vi-VN/common.po';
+
+const localesData: TJSON = {
+  viVN,
+};
 
 export const gt: GetText = new GetText();
 
-const localesData: TJSON = {
-  [locales[0]]: uaLocale,
-  [locales[1]]: ruLocale,
-};
-
-locales.forEach((locale: string) => {
-  gt.addTranslations(locale, defaultLocaleDomain, localesData[locale]);
+locales.forEach((locale: string): void => {
+  gt.addTranslations(
+    locale,
+    defaultLocaleDomain,
+    localesData[locale.replace('-', '')]
+  );
 });
 
 gt.setTextDomain(defaultLocaleDomain);
